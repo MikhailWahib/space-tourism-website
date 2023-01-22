77d51289bf95f971db6a78f337c7171fe9f37d04
MikhailWahib <mikhailwahib1@gmail.com>
2023-01-22 22:06:56 +0200

    resolving netlify issue #2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   historyApiFallback: {
     rewrites: [
-      { from: /^\/[^\.]*$/, to: '/index.html' },
+      { from: /^\/[^\.]*$/, to: '/' },
     ]
   }
 })
