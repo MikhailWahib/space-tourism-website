5e74fbdfeecb948df1752a22de4fd5c681de91ef
MikhailWahib <mikhailwahib1@gmail.com>
2023-01-23 15:24:50 +0200

    Transition effects added

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import moonImg from '../assets/destination/image-moon.png'
 import marsImg from '../assets/destination/image-mars.png'
 import europaImg from '../assets/destination/image-europa.png'
@@ -25,11 +25,18 @@ export default function Destination({ data }) {
                 <h5 className="heading-5 text-white text-start"><span className='font-bold opacity-25'>01</span> Pick your destination</h5>
                 <div className="lg:flex lg:flex-row lg:justify-between">
                     <div className="dest-img py-8 lg:w-fit">
-                        <img
-                            src={imgs[current]}
-                            alt="moon"
-                            className='min-h-[220px] min-w-[220px] max-h-[445px] max-w-[445px] h-[30vmax] mx-auto'
-                        />
+                        {imgs.map((im, i) => {
+                            return <motion.img
+                                key={`image-${i}-${current}`}
+                                src={im}
+                                alt={data[current].name}
+                                className={`min-h-[220px] min-w-[220px] max-h-[445px] max-w-[445px] h-[30vmax] mx-auto ${current === i ? 'block' : 'hidden'}`}
+
+                                initial={{ opacity: .25 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: .5 }}
+                            />
+                        })}
                     </div>
                     <div className='lg:w-[42%]'>
                         <div className="dest-btns flex justify-center gap-x-6 md:my-5 lg:justify-start lg:gap-x-12">
@@ -45,22 +52,30 @@ export default function Destination({ data }) {
                             })}
 
                         </div>
-                        <h3 className="heading-3 my-4 md:text-[5rem] md:my-5 lg:text-start lg:text-[6.25rem]">{data[current].name}</h3>
-                        <p className="body-text h-32 text-[0.9375rem] mb-9 md:text-[1.125rem] lg:text-[1.125rem] lg:text-start">{data[current].description}</p>
-                        <hr className='opacity-25' />
-                        <div className="mt-5 flex flex-col gap-y-5 md:flex-row md:justify-center md:gap-x-24 md:mt-10 lg:justify-start">
-                            <div>
-                                <p className="subheading-2">Avg. Distance</p>
-                                <p className="subheading-1">{data[current].distance}</p>
+                        <motion.div
+                            key={`planet-${current}`}
+
+                            initial={{ opacity: .25 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: .5 }}
+                        >
+                            <h3 className="heading-3 my-4 md:text-[5rem] md:my-5 lg:text-start lg:text-[6.25rem]">{data[current].name}</h3>
+                            <p className="body-text h-32 text-[0.9375rem] mb-9 md:text-[1.125rem] lg:text-[1.125rem] lg:text-start">{data[current].description}</p>
+                            <hr className='opacity-25' />
+                            <div className="mt-5 flex flex-col gap-y-5 md:flex-row md:justify-center md:gap-x-24 md:mt-10 lg:justify-start">
+                                <div>
+                                    <p className="subheading-2">Avg. Distance</p>
+                                    <p className="subheading-1">{data[current].distance}</p>
+                                </div>
+                                <div>
+                                    <p className="subheading-2">Est. Travel Time</p>
+                                    <p className="subheading-1">{data[current].travel}</p>
+                                </div>
                             </div>
-                            <div>
-                                <p className="subheading-2">Est. Travel Time</p>
-                                <p className="subheading-1">{data[current].travel}</p>
-                            </div>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
-            </div>
-        </motion.section>
+            </div >
+        </motion.section >
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import logo from '../assets/shared/logo.svg'
 import menuIcon from '../assets/shared/icon-hamburger.svg'
 import closeIcon from '../assets/shared/icon-close.svg'
@@ -13,17 +14,42 @@ export default function Navbar() {
 
     return (
         <header className='w-full z-50 absolute flex justify-between items-center p-[1.5rem] lg:p-0 lg:py-5 lg:mt-5'>
-            <div className="logo md:ml-[2.5rem]"><img src={logo} alt="Logo" /></div>
-            <button className="mobile-menu cursor-pointer md:hidden" onClick={handleToggle}><img src={menuIcon} alt="menu" /></button>
-            <nav className={` ${showMenu ? 'block' : 'hidden'} absolute top-0 right-0 h-screen w-[72%] bg-white bg-opacity-[0.04] backdrop-blur-[40.7742px] p-[2.125rem] md:w-[60%] md:block md:p-0 md:h-[6rem]`}>
-                <button className="block w-fit ml-auto cursor-pointer md:hidden" onClick={handleToggle}><img src={closeIcon} alt="close" className='w-[19px]' /></button>
+
+            <div className="logo md:ml-[2.5rem]">
+                <img src={logo} alt="Logo" />
+            </div>
+
+            <button className="mobile-menu cursor-pointer md:hidden" onClick={handleToggle}>
+                <img src={menuIcon} alt="menu" />
+            </button>
+
+            <motion.nav
+                key={showMenu}
+                className={` ${showMenu ? 'block' : 'hidden'} absolute top-0 -right-20 h-screen w-[72%] bg-white bg-opacity-[0.04] backdrop-blur-[40.7742px] p-[2.125rem] md:right-0 md:w-[60%] md:block md:p-0 md:h-[6rem]`}
+
+                animate={{ right: 0 }}
+                transition={{ type: 'keyframes' }}
+            >
+
+                <button className="block w-fit ml-auto cursor-pointer md:hidden" onClick={handleToggle}>
+                    <img src={closeIcon} alt="close" className='w-[19px]' />
+                </button>
+
                 <ul className='nav-text text-[1.3rem] w-fit m-auto flex flex-col gap-y-[2rem] mt-10 md:relative md:flex-row md:justify-around md:items-center md:w-full md:h-full md:gap-y-0 md:mt-0 md:px-5 md:text-[1.2rem]'>
-                    <li onClick={handleToggle} className={location.pathname === '/' ? 'active-nav' : ''}><Link to='/'><span className='nav-number pr-2 md:hidden lg:inline'>00</span> Home</Link></li>
-                    <li onClick={handleToggle} className={location.pathname === '/destination' ? 'active-nav' : ''}><Link to='/destination'><span className='nav-number pr-2 md:hidden lg:inline'>01</span> Destination</Link></li>
-                    <li onClick={handleToggle} className={location.pathname === '/crew' ? 'active-nav' : ''}><Link to='/crew'><span className='nav-number pr-2 md:hidden lg:inline'>02</span> Crew</Link></li>
-                    <li onClick={handleToggle} className={location.pathname === '/technology' ? 'active-nav' : ''}><Link to='/technology'><span className='nav-number pr-2 md:hidden lg:inline'>03</span> Technology</Link></li>
+                    <li onClick={handleToggle} className={location.pathname === '/' ? 'active-nav' : ''}>
+                        <Link to='/'><span className='nav-number pr-2 md:hidden lg:inline'>00</span> Home</Link>
+                    </li>
+                    <li onClick={handleToggle} className={location.pathname === '/destination' ? 'active-nav' : ''}>
+                        <Link to='/destination'><span className='nav-number pr-2 md:hidden lg:inline'>01</span> Destination</Link>
+                    </li>
+                    <li onClick={handleToggle} className={location.pathname === '/crew' ? 'active-nav' : ''}>
+                        <Link to='/crew'><span className='nav-number pr-2 md:hidden lg:inline'>02</span> Crew</Link>
+                    </li>
+                    <li onClick={handleToggle} className={location.pathname === '/technology' ? 'active-nav' : ''}>
+                        <Link to='/technology'><span className='nav-number pr-2 md:hidden lg:inline'>03</span> Technology</Link>
+                    </li>
                 </ul>
-            </nav>
+            </motion.nav>
         </header>
     )
 }
